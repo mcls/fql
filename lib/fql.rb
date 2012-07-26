56a0af6c9d8627073fa287eb58a315a455adcdd7
Maarten Claes <maartencls@gmail.com>
2012-07-26 08:36:17 +0200

    Fix typo

--- lib/fql.rb
+++ lib/fql.rb
@@ -6,13 +6,14 @@ module Fql
   BASE_URL = 'https://graph.facebook.com/fql?q='
 
   def self.execute(fql_query, options = {})
-    url = make_url(fql_query, options)
+    query = Fql::Query.new fql_query
+    url = make_url(query, options)
     response = make_request url 
     self.decode_response response
   end
 
-  def self.make_url(fql_query, options = {})
-    url = self::BASE_URL + URI.encode(fql_query.compose)
+  def self.make_url(query, options = {})
+    url = self::BASE_URL + URI.encode(query.compose)
     if options.has_key?(:access_token)
       url += "&access_token=#{options[:access_token]}"
     end
