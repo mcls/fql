2d3018665f59f6f5e54e04291977e5157927f033
Alex Rozumey <brain-geek@yandex.ua>
2013-04-10 11:51:06 +0300

    Add possibility to get FB response from exception.

--- lib/fql/exception.rb
+++ lib/fql/exception.rb
@@ -1,10 +1,11 @@
 class Fql::Exception < Exception
 
-  attr_reader :type
+  attr_reader :type, :decoded_json
 
   def initialize(decoded_json)
     error = decoded_json["error"]
     @type = error["type"]
+    @decoded_json = decoded_json
     super(error["message"])
   end
 
